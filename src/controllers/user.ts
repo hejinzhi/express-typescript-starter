41a800d3a4129312ed47e5970b52006dd3e84cc6
hejinzhi <504151911@qq.com>
2021-05-12 18:04:51 +0800

    删除无用代码

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response,  } from "express";
 export const getUserList = (req: Request, res: Response): void => {
   //   return "hello world";
-  res.send("hello world");
+  res.send("hello world111");
 };
